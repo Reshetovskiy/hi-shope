d58d6af0f59b4b645dc9ee712b8fbe23bbd70454
Yana <yana.lihotska@gmail.com>
2018-12-26 12:49:15 +0200

    [+]добавила карту

--- src/js/partials/maps.js
+++ src/js/partials/maps.js
@@ -1,212 +1,138 @@
-// ;(function ($) {
-//     var latitude = 55.754454,
-//     longitude = 37.523892,
-//     map_zoom = 15;
-
-//     var style= [
-//     {
-//         "featureType": "all",
-//         "elementType": "labels.text.fill",
-//         "stylers": [
-//             {
-//                 "saturation": 36
-//             },
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 40
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "all",
-//         "elementType": "labels.text.stroke",
-//         "stylers": [
-//             {
-//                 "visibility": "on"
-//             },
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 16
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "all",
-//         "elementType": "labels.icon",
-//         "stylers": [
-//             {
-//                 "visibility": "off"
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "administrative",
-//         "elementType": "geometry.fill",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 20
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "administrative",
-//         "elementType": "geometry.stroke",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 17
-//             },
-//             {
-//                 "weight": 1.2
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "landscape",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 20
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "poi",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 21
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road.highway",
-//         "elementType": "geometry.fill",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 17
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road.highway",
-//         "elementType": "geometry.stroke",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 29
-//             },
-//             {
-//                 "weight": 0.2
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road.arterial",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 18
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "road.local",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 16
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "transit",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 19
-//             }
-//         ]
-//     },
-//     {
-//         "featureType": "water",
-//         "elementType": "geometry",
-//         "stylers": [
-//             {
-//                 "color": "#000000"
-//             },
-//             {
-//                 "lightness": 17
-//             }
-//         ]
-//     }
-//     ]
-//     if($('.contacts').data('page') == 'contacts'){
-//     //Создание точки на карте
-//     var map_options = {
-//         center: new google.maps.LatLng(latitude, longitude),
-//         zoom: map_zoom,
-//         panControl: false,
-//         zoomControl: false,
-//         mapTypeControl: false,
-//         streetViewControl: false,
-//         mapTypeId: google.maps.MapTypeId.ROADMAP,
-//         scrollwheel: false,
-//         styles: style
-//     }
-//     //Инициализация карты
-//     var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+;(function ($) {
+    var latitude = 55.759616,
+    longitude = 37.625457,
+    map_zoom = 18;
 
 
-//     //Добавляем свои иконки для кнопок увеличить/уменьшить на карту
-//     function CustomZoomControl(controlDiv, map) { 
-//         var controlUIzoomIn= document.getElementById('zoom-in'),
-//             controlUIzoomOut= document.getElementById('zoom-out');
-//         controlDiv.appendChild(controlUIzoomIn);
-//         controlDiv.appendChild(controlUIzoomOut);
+  
 
-//         //Делаем привязку для кнопок увеличить/уменьшить при клике
-//         google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-//             map.setZoom(map.getZoom()+1)
-//         });
-//         google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-//             map.setZoom(map.getZoom()-1)
-//         });
-//     }
+    var style= [
+        {
+            "featureType": "all",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "gamma": "0.4"
+                },
+                {
+                    "saturation": "-86"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "geometry",
+            "stylers": [
+                {
+                    "saturation": "-31"
+                },
+                {
+                    "lightness": "15"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "geometry.fill",
+            "stylers": [
+                {
+                    "saturation": "-99"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels",
+            "stylers": [
+                {
+                    "saturation": "-63"
+                },
+                {
+                    "gamma": "1.14"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text",
+            "stylers": [
+                {
+                    "gamma": "0.92"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.fill",
+            "stylers": [
+                {
+                    "gamma": "1.29"
+                },
+                {
+                    "saturation": "-6"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.text.stroke",
+            "stylers": [
+                {
+                    "gamma": "1.00"
+                }
+            ]
+        },
+        {
+            "featureType": "all",
+            "elementType": "labels.icon",
+            "stylers": [
+                {
+                    "weight": "0.00"
+                },
+                {
+                    "gamma": "2"
+                },
+                {
+                    "lightness": "-29"
+                },
+                {
+                    "saturation": "-82"
+                }
+            ]
+        },
+        {
+            "featureType": "administrative.country",
+            "elementType": "all",
+            "stylers": [
+                {
+                    "gamma": "1.00"
+                }
+            ]
+        }
+    ]
 
-//     var zoomControlDiv = document.createElement('div');
-//     var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+    if($('.map').data('page') == 'contacts'){
 
-//     //Помещаем кнопки увеличить/уменьшить на карту в левый верхний угол
-//     map.controls[google.maps.ControlPosition.LEFT_BOTTOM].push(zoomControlDiv);
-// }
-// })(jQuery)
+        var map_options = {
+            center: new google.maps.LatLng(latitude, longitude),
+            zoom: map_zoom,
+            panControl: false,
+            zoomControl: false,
+            mapTypeControl: false,
+            streetViewControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false,
+            styles: style
+        }
+
+        var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+
+        var marker = new google.maps.Marker ({
+            map: map,
+            position: {lat: 55.759616, lng: 37.625457},
+            icon: 'img/marker.svg'
+        })
+    }
+
+   
+})(jQuery)
